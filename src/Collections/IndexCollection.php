3ae9a03b51c485a673103594c18f059914339d2a
Ashan Ghimire <ashan.ghimire@javra.com>
2019-11-06 10:30:19 +0545

    create index collection class

---
+++ src/Collections/IndexCollection.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * IndexCollection
+ *
+ * @copyright Copyright © 2019 Javra Softwares. All rights reserved.
+ * @author Ashan Ghimire  <ashan.ghimire@javra.com>
+ */
+
+
+namespace Elastic\Collections;
+
+class IndexCollection extends AbstractCollection
+{
+
+    public static function buildFromArray(array $array)
+    {
+        
+    }
+
+}
